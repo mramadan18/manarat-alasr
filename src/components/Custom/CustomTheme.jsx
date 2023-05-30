4359875773bc227bba52d2ac6bd89b75ef059354
mramadan18 <m.ramadan181199@gmail.com>
2023-05-30 11:00:59 +0300

    remove useEffect from custom theme page

--- src/components/Custom/CustomTheme.jsx
+++ src/components/Custom/CustomTheme.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { faCog, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CustomMode from "./CustomMode";
